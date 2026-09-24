ea07c38afacd6d6cfe0fd339986f08f0b662b7f0
Mark-TWT <marketing@twt-gmbh.de>
2026-09-24 15:33:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -28,8 +28,9 @@ function downloadPdf() {
 
     const link = document.createElement("a");
 
-    link.href = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN ONEPAGER ENG.pdf";
-    link.download = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN ONEPAGER ENG.pdf";
+link.href = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN-ONEPAGER-ENG.pdf";
+link.download = "MULTI-LEVEL-INVERTER-ARCHITEKTUREN-ONEPAGER-ENG.pdf";
+
 
     document.body.appendChild(link);
     link.click();
